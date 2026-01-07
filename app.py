91ee7eb183eeddef6f056a589a2a0750f2a7d506
jikang-jeong <jikjeong123@gmail.com>
2026-01-07 09:48:19 +0000

    update

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
- 
+
+import signal
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html", message="hi")
+    return render_template("index.html", message="Hello World!222")
 
 @app.route("/health")
 def health():
     return "SUCCESS", 200
 
 @app.route("/error")
-def oom():
+def error():
     return "Internal Server Error", 500
 
 if __name__ == "__main__":
